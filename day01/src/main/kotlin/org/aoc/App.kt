df440c1b4915b30b66b1acb72e7225610c2c0f23
Michael Cohen <michael.cohen@globality.com>
2022-11-30 22:06:23 -0800

    Day 01 Solution

--- day01/src/main/kotlin/org/aoc/App.kt
+++ day01/src/main/kotlin/org/aoc/App.kt
@@ -1,5 +1,34 @@
 package org.aoc
 
+import java.io.File
+
+fun processInput(lines: List<String>): List<List<Int>> {
+    return lines.fold(mutableListOf(mutableListOf<Int>())) { groups, line ->
+        if (line.isEmpty()) {
+            groups.add(mutableListOf())
+        } else {
+            groups.last().add(line.toInt())
+        }
+        groups
+    }
+}
+
+fun runSolutionPart1(input: List<List<Int>>) {
+    val answer = input.sortedBy { it.sum() }.last().sum()
+    println("*** Solution for Day 1, part 1")
+    println("answer: $answer")
+}
+
+fun runSolutionPart2(input: List<List<Int>>) {
+    val answer = input.sortedBy { it.sum() }.takeLast(3).sumOf { it.sum() }
+    println("*** Solution for Day 1, part 2")
+    println("answer: $answer")
+}
+
 fun main() {
-    println("Hello")
+    val rawInput = File("day01/src/main/resources/input.txt").readLines()
+    val input = processInput(rawInput)
+
+    runSolutionPart1(input)
+    runSolutionPart2(input)
 }
