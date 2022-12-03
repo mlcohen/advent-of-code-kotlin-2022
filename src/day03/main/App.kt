d104747ae861cc8249e4b268173542935240ca56
Michael Cohen <michael.cohen@globality.com>
2022-12-02 21:56:52 -0800

    Day 03 Solution

--- src/day03/main/App.kt
+++ src/day03/main/App.kt
@@ -1,5 +1,48 @@
 package day03.main
 
+import java.io.File
+
+fun runSolutionPart1(input: List<String>) {
+    println("*** Solution for Day 2, part 1")
+
+    val sum = input.map { items ->
+            val firstCompartment = items.subSequence(0, items.length / 2).toSet()
+            val secondCompartment = items.subSequence((items.length / 2), items.length).toSet()
+            firstCompartment.intersect(secondCompartment).first()
+        }
+        .map { item ->
+            val priority = when(item) {
+                in 'a'..'z' -> item - 'a' + 1
+                in 'A'..'Z' -> item - 'A' + 27
+                else -> throw error("Invalid item ${item}")
+            }
+            (item to priority)
+        }
+        .sumOf { it.second }
+
+    println("sum = $sum")
+}
+
+fun runSolutionPart2(input: List<String>) {
+    println("*** Solution for Day 2, part 2")
+
+    val sum = input.chunked(3)
+        .map { group -> group.map { it.toSet() }.reduce { acc, item -> acc.intersect(item) }.first() }
+        .map { item ->
+            val priority = when(item) {
+                in 'a'..'z' -> item - 'a' + 1
+                in 'A'..'Z' -> item - 'A' + 27
+                else -> throw error("Invalid item ${item}")
+            }
+            (item to priority)
+        }
+        .sumOf { it.second }
+
+    println("sum = $sum")
+}
+
 fun main() {
-    println("Day 3")
+    val input = File("src/day03/resources/input.txt").readLines()
+    runSolutionPart1(input)
+    runSolutionPart2(input)
 }
